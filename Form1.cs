6260a785776a3d60bbb9986d0737c038e75c1ae5
eltro <eltro@Advance>
2023-03-27 22:37:31 +0000

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,28 @@
+using MaterialSkin;
+using MaterialSkin.Controls;
+
+namespace Demo1
+{
+    public partial class Form1 : MaterialForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            var materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
+            materialSkinManager.ColorScheme = new ColorScheme(Primary.BlueGrey800, Primary.BlueGrey900, Primary.BlueGrey500, Accent.LightBlue200, TextShade.WHITE);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
